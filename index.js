387e90c2df1c3654173a8cd1dac873c9bc1d5b8b
Gray Hanna <ghostblossom54288@gmail.com>
2024-05-14 15:13:37 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(arrayOfNames, eventName){
+    const messages =[]
+    for(let i =0; i < arrayOfNames.length; i++){
+        console.log(`Thank you, ${arrayOfNames[i]}, for the wonderful ${eventName} gift!`)
+        messages.push(`Thank you, ${arrayOfNames[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return messages
+}
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function countDown(number){
+
+    while(number >= 0){
+        console.log(number--)
+    }
+    
+}
